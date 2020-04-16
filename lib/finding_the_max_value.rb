137fb125f7eee9a3706ea7e656269fb8b2b0c333
Bryce Kennedy <btken88@gmail.com>
2020-04-16 20:24:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,9 @@
 def find_max_value(array)
-  max_value = nil
-  for 
+  max_value = 0
+  for i in array
+    if array[i] > max_value
+      max_value = array[i]
+    end
+  end
+  return max_value
 end
